42b7d5f4ca652dc3812d0efb69567ff5141aee6d
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-19 22:42:39 +0400

    feat: setup seeder for Book model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Book;
+use App\Models\Review;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    private const RATING_BETWEENS = [
+        [1, 2],
+        [2, 4],
+        [4, 5],
+    ];
+
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
         // User::factory(10)->create();
+        Book::factory(50)->create()->each(function (Book $book) {
+            $reviewCount = random_int(0, 50);
+            $ratingBetween = self::RATING_BETWEENS[array_rand(self::RATING_BETWEENS)];
+
+            Review::factory($reviewCount)->ratingBetween($ratingBetween)->bookCreatedAt($book->created_at)->for($book)->create();
+        });
 
         User::factory()->create([
             'name' => 'Test User',

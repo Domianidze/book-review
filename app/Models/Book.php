07096e32cc379374d436f14a37f369e3627bd96b
Sandro Domianidze <sdomianidze123@gmail.com>
2024-06-20 16:45:42 +0400

    feat(Book): create scopeSearch method

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function reviews () {
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeSearch(Builder $query, string $value)
+    {
+        return $query->where('title', 'LIKE', '%' . $value . '%');
     }
 }
